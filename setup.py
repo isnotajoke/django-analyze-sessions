1b7409d516c65bd4291c410dd42d8640bd695e07
Kevan Carstensen <kevan@isnotajoke.com>
2013-08-21 14:30:49 -0700

    New release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-analyze-sessions',
-    version='0.1',
+    version='0.1.1',
     author='Kevan Carstensen',
     author_email='kevan@isnotajoke.com',
     packages=['analyze_sessions', 'analyze_sessions.management', 'analyze_sessions.management.commands'],
